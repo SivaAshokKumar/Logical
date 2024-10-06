cd125d6390e5a44e472bf5b156756d562af88a96
SivaAshokKumar <sivaashokkumar412@gmail.com>
2024-10-06 14:34:25 +0530

    Commited On Sunday First I initialize the for then after i used CHATGPT I know i should usenested for loop to return the answer for the 1st Leetcode Problem.

---
+++ src/main/java/Siva/Java/Practise/Arrays_Practise/TwoSUm_Leetcode.java
@@ -0,0 +1,35 @@
+package Siva.Java.Practise.Arrays_Practise;
+
+import java.util.Arrays;
+
+public class TwoSUm_Leetcode {
+
+        //THE MISTAKE IN THE BELOW CODE IS NOT USING NESTED LOOP
+      /*  if(target > 0){
+            for(int i =0 ;i < arr.length - 1; i++){
+                int sum =  arr[i]+arr[i+1];
+                if(sum == target){
+                    //System.out.println(arr[i]+" "+arr[i+1]);
+                    System.out.println(i+ " " + (i+1) );
+                                    }
+            }
+        }*/
+        public static int TwoSUm(int arr[], int target){
+            for (int i = 0; i < arr.length - 1; i++) {
+                for (int j = i + 1; j < arr.length; j++) {
+                    int sum = arr[i] + arr[j];
+                    if (sum == target) {
+                        return new int[] {i, j};
+                    }
+                }
+            }
+            return new int[]{};
+        }
+    public static void main(String[] args) {
+        int arr[] = {4,7,2,8,6};
+        int tar = 8;
+        int[] f =  TwoSUm(arr, tar);
+        System.out.println(f);
+
+    }
+}
